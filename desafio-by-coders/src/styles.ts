7a4fcd63210f426f6dec88c81c2a22adb66fd61f
gojnimer <gojnimer@gmail.com>
2022-04-01 23:42:21 -0300

    feat: added global styles

---
+++ desafio-by-coders/src/styles.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  html {
+    height: 100vh;
+  }
+  body {
+    margin: 0px 0px;
+    padding: 0px 0px;
+  }
+  * {
+      font-family: system-ui;
+    box-sizing: border-box;
+  }
+
+`;
