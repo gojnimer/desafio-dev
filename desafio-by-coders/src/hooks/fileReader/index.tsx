6aa24462db4286f237f3d82de4fa92b1b931cc54
gojnimer <gojnimer@gmail.com>
2022-04-02 22:46:18 -0300

    feat: created a helper hook for file parsing and added default file events for cnab parsing

--- desafio-by-coders/src/hooks/fileReader/index.tsx
+++ desafio-by-coders/src/hooks/fileReader/index.tsx
@@ -1,11 +1,32 @@
-import { useState } from "react";
-import { ICnabValues } from "../../interfaces";
+import {  useState } from "react";
+import { ICnabValues, IReturnFileReader } from "../../interfaces";
 import { processCNAB } from "../../utils";
 
-export const useFileReader = () => {
-  const [previewData, setPreviewData] = useState<Array<ICnabValues | string>>();
-  const cnabFileHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
+export const useFileReader = (): IReturnFileReader => {
+  const [previewData, setPreviewData] = useState<Array<ICnabValues>>();
+  const cnabFileDropHandler = async (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
+    e.stopPropagation();
+    if (e.dataTransfer?.files?.length) {
+      const process: Array<Array<ICnabValues | string> | boolean> =
+        await Promise.all(
+          Array.from(e.dataTransfer.files).map(
+            async (file: any) => await processCNAB(file).catch(() => false)
+          )
+        );
+      if (process.filter((x) => x).length) {
+        const result = process
+          .filter((x) => x !== false && typeof x !== "string")
+          .reduce((a: any[], b) => a.concat(b), []) as Array<ICnabValues>;
+        setPreviewData(result);
+      }
+    }
+  };
+  const cnabFileClickHandler = async (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    e.preventDefault();
+    e.stopPropagation();
     if (e.target?.files?.length) {
       const process: Array<Array<ICnabValues | string> | boolean> =
         await Promise.all(
@@ -14,18 +35,18 @@ export const useFileReader = () => {
           )
         );
       if (process.filter((x) => x).length) {
+        console.log(process);
         const result = process
-          .filter((x) => x !== false)
-          .reduce((a: any[], b) => a.concat(b), []) as Array<
-          ICnabValues | string
-        >;
-        console.log(result);
+          .filter((x) => x !== false && typeof x !== "string")
+          .reduce((a: any[], b) => a.concat(b), []) as Array<ICnabValues>;
         setPreviewData(result);
       }
     }
   };
   return {
-    cnabFileHandler,
+    cnabFileDropHandler,
+    cnabFileClickHandler,
     previewData,
+    setPreviewData,
   };
 };
--- desafio-by-coders/src/utils/hooks/fileReader/index.ts
+++ desafio-by-coders/src/utils/hooks/fileReader/index.ts
@@ -1,4 +1,6 @@
-import { ICnabValues } from "../../../interfaces";
+/* eslint-disable no-eval */
+import { transactionOperators } from "../../../constants/cnab";
+import { ICnabStoreValues, ICnabValues } from "../../../interfaces";
 
 const processCNAB = (cnabTxt: Blob): Promise<Array<ICnabValues | string>> =>
   new Promise((resolve, reject) => {
@@ -15,11 +17,11 @@ const processCNAB = (cnabTxt: Blob): Promise<Array<ICnabValues | string>> =>
           return {
             type: Number(line.substring(0, 1)),
             date: line.substring(1, 9),
-            value: (Number(line.substring(9, 19).trim()) / 100),
+            value: Number(line.substring(9, 19).trim()) / 100,
             ownerDocument: line.substring(19, 30),
             bankCard: line.substring(30, 42),
             timestamp: line.substring(42, 48),
-            shopName: line.substring(48, 62).trim(),
+            storeName: line.substring(48, 62).trim(),
             ownerName: line.substring(62, 81).trim(),
           };
         });
@@ -30,4 +32,55 @@ const processCNAB = (cnabTxt: Blob): Promise<Array<ICnabValues | string>> =>
     reader.readAsText(cnabTxt);
   });
 
-export { processCNAB };
+const mapCnabByStore = (cnabList: ICnabValues[]): ICnabStoreValues[] => {
+  return cnabList.reduce(
+    (
+      prev: ICnabStoreValues[],
+      { storeName, ownerDocument, ownerName, ...current }: ICnabValues
+    ) => {
+      const index = prev.findIndex(
+        ({
+          storeName: previewStoreName,
+          ownerDocument: previewOwnerDocument,
+        }) =>
+          previewStoreName === storeName &&
+          previewOwnerDocument === ownerDocument
+      );
+      if (index === -1) {
+        prev.push({
+          storeName,
+          ownerName,
+          ownerDocument,
+          transactions: [{ ...current }],
+          currentBalance: eval(
+            `0 ${transactionOperators[current.type]} ${current.value}`
+          ),
+        });
+      } else {
+        prev[index].currentBalance = eval(
+          `${prev[index].currentBalance} ${
+            transactionOperators[current.type]
+          } ${current.value}`
+        );
+        prev[index].transactions.push(current);
+      }
+      return prev;
+    },
+    []
+  ) as ICnabStoreValues[];
+};
+
+const formatCnabDate = (date: string, timestamp: string) => {
+  const data = Date.parse(
+    `${date.substring(4, 6)}/${date.substring(6, 8)}/${date.substring(
+      0,
+      4
+    )} ${timestamp.substring(0, 2)}:${timestamp.substring(
+      2,
+      4
+    )}:${timestamp.substring(4, 6)}  GMT-0300`
+  );
+  return new Date(data).toLocaleString();
+};
+
+export { processCNAB, mapCnabByStore, formatCnabDate };
