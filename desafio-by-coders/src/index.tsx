ebbc801aa07b4c61945af9d4d645360bdc0b2bb9
gojnimer <gojnimer@gmail.com>
2022-04-04 04:48:07 -0300

    feat: updated index.tsx with Context providers

--- desafio-by-coders/src/index.tsx
+++ desafio-by-coders/src/index.tsx
@@ -1,12 +1,18 @@
 import * as ReactDOMClient from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Routing } from "./components";
+import { CnabProvider } from "./contexts/cnabContext";
+import { MessageCenterProvider } from "./contexts/messageCenter";
 
 const rootElement = document.getElementById("root") as any;
 
 const root = ReactDOMClient.createRoot(rootElement);
 root.render(
   <BrowserRouter>
-    <Routing />
+    <MessageCenterProvider>
+      <CnabProvider>
+        <Routing />
+      </CnabProvider>
+    </MessageCenterProvider>
   </BrowserRouter>
 );
