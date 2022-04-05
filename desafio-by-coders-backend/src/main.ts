782b79bba8f74e8d93424fa507efef6912595489
gojnimer <gojnimer@gmail.com>
2022-04-05 00:56:39 -0300

    feat: added swagger

--- desafio-by-coders-backend/src/main.ts
+++ desafio-by-coders-backend/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Should disable cors for production
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const config = new DocumentBuilder()
+    .setTitle('DESAFIO BY CODERS')
+    .setDescription('API created for By Coders challenge')
+    .setVersion('1.0')
+    .addTag('CNAB')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
