dd9ea47f564848650f9b96f58848143cb5d8e8b0
gojnimer <gojnimer@gmail.com>
2022-04-05 00:54:30 -0300

    feat: added testbed utils

---
+++ desafio-by-coders-backend/src/test/testBed/mock.ts
@@ -0,0 +1,38 @@
+/* eslint-disable prettier/prettier */
+export const mock = {
+  defaultTransactionsList: [
+    {
+      id: 1,
+      type: 1,
+      date: '20190301',
+      timestamp: '153453',
+      value: 142,
+      ownerDocument: '09620676017',
+      bankCard: '4753****3153',
+      ownerName: 'Testing Store',
+      storeName: 'John Doe',
+    },
+    {
+      id: 2,
+      type: 2,
+      date: '20190301',
+      timestamp: '145607',
+      value: 132,
+      ownerDocument: '55641815063',
+      bankCard: '3123****7687',
+      ownerName: 'Testing Store',
+      storeName: 'John Doe',
+    },
+    {
+      id: 3,
+      type: 3,
+      date: '20190301',
+      timestamp: '172712',
+      value: 122,
+      ownerDocument: '84515254073',
+      bankCard: '6777****1313',
+      ownerName: 'Store Testing',
+      storeName: 'Jane Doe',
+    },
+  ],
+};
