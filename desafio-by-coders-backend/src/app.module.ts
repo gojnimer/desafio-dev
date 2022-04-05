f56e7b569bd47e27019d561f5c8270429094ef45
gojnimer <gojnimer@gmail.com>
2022-04-05 00:58:18 -0300

    feat: updated default module to load db credentials from env

--- desafio-by-coders-backend/src/app.module.ts
+++ desafio-by-coders-backend/src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TerminusModule } from '@nestjs/terminus';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HealthController } from './health/health.controller';
+import { CnabController } from './cnab/cnab.controller';
+import { CnabService } from './cnab/cnab.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TerminusModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        // entities: ['src/**/*.entity{.ts,.js}'], << config for serverless deploy
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        ssl: { rejectUnauthorized: true },
+        synchronize: false,
+        migrations: ['src/migration/**/*.js'],
+        migrationsRun: true,
+        cli: {
+          entitiesDir: 'src/app/models',
+          migrationsDir: 'src/migration',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [HealthController, CnabController],
+  providers: [CnabService],
 })
 export class AppModule {}
